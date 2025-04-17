4598ce14ad6533e96888ec06353a528bbf069ea5
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-04-17 19:18:43 +0700

    layout

--- week1/src/App.jsx
+++ week1/src/App.jsx
@@ -1,32 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
+import Home from "./components/ex_week5/Home";
 
-import "./Responsive.scss"
-import Port2 from "./components/Port2/Port2";
-import Countdown from "./components/Countdown/Countdown";
-import Prop_demo from "./components/Prop_demo/Prop_demo";
-import Order from "./components/order/order";
+
+
 function App() {  
-  const [isOn, setIsOn] = useState(true);
-    const handleEvent = () => {
-        setIsOn(!isOn)
-    }
-    console.log(isOn);
-  const [display, setDisplay] = useState(false);
-  return (
+  return(
     <>
-      {/* <Port2/> */}
-      <button onClick={() => setDisplay(!display) }> Toggle</button>
-      {/* {display && <Port2/>} */}
-      {/* {display && <Countdown/>} */}
-
-      <p>========================</p>
-      <Prop_demo/>
-      {/* <Countdown/> */}
-
-      <Order/>
+    <Home/>
     </>
-  );
+  )
+  
 }
 
 export default App;
---
+++ week1/src/components/ex_week5/Data.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <>
+    `</>
+  )
+}
+
+export default Data
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=VietNam&appid=9f70a3cd6c6c1218240e07fc1be919bc&units=metric&lang=vi
+
+// https://api.openweathermap.org/data/2.5/forecast?q=London&appid=9f70a3cd6c6c1218240e07fc1be919bc
