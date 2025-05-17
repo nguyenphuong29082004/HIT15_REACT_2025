85fed5145c1d86fa1f4239eb53dee48431a83d71
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-05-17 19:55:40 +0700

    fix vercel

--- week1/src/App.jsx
+++ week1/src/App.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./components/header/Header";
-import Banner from "./components/Banner/Banner";
-import Session from "./components/Session/Session";
-import Describe from "./components/Describe/Describe";
-import Review from "./components/Review/Review";
-import Footer from "./components/Footer/Footer";
-import Register from "./components/Register/Register";
 import {useRoutes} from "react-router-dom";
 import routes from "./components/Router/Router";
 function App() {  
